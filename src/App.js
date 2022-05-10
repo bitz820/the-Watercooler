f59a59e221bbdb29defc9f6bf38494bb9c89ffe0
Mark <mark.n.bacon.jr@gmail.com>
2022-05-09 23:30:06 -0400

    Created basic routing and additional pages for each component, waiting on the matches page from Sam.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import react from "react"
+import React from "react"
+import { Route, Switch } from 'react-router-dom'
+import NavBar from "./NavBar"
 import User from "./User"
+import LoverProfile from "./LoverProfile"
+// import Match from "./Match"
 
 function App() {
 
   return (
     <div className="App">
-      <User />
+      <NavBar />
+      <Switch>
+        <Route exact path="/">
+          <User />
+        </Route>
+        <Route path="/loverProfile">
+          <LoverProfile />
+        </Route>
+        {/* <Route path="/matches">
+          <Match/>
+        </Route> */}
+      </Switch>
     </div>
   );
 }
