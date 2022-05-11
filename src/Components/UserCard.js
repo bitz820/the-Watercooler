9a8bbd2e7e74fa48bc66e7301836726a658e0b88
sam9750 <sam.wadhwa95@gmail.com>
2022-05-11 19:21:47 -0400

    like button, css edits

--- src/Components/UserCard.js
+++ src/Components/UserCard.js
@@ -1,15 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
+import "../index.css"
 
 function UserCard({userInfo}) {
   console.log(userInfo)
+  const [liked, setLiked] = useState( false )
+
+  const toggleLike = () => {
+    setLiked( !liked )
+  }
   return (
-    <div key={userInfo.id}>
+    <div className="ui-card"
+        key={userInfo.id}>
             <h1>{userInfo.username}</h1>
             <img src={userInfo.profile_picture} alt="My Profile"/>
             <h3>About Me: {userInfo.bio}</h3>
             <h5>Gender: {userInfo.gender}</h5>
             <p>Interests: {userInfo.interests}</p>
             {/* <h4>Age: USER.BIRTDATE BUT USE THE AGE METHOD TO CALCULATE?</h4> */}
+            {liked ? (
+          <button className="ui-card" onClick={toggleLike} className="emoji-button favorite active">❤️</button>
+        ) : (
+          <button className="ui-card" onClick={toggleLike} className="emoji-button favorite">♡</button>
+        )}
     </div>
   )
 }
